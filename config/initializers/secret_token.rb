bcfd5106b50983f9ad10556caed2abb3904a7257
Brandon Cox <brandon.cox.519@gmail.com>
2013-06-29 06:18:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angular::Application.config.secret_key_base = '63f5e51db98b3629b3773f49747f72bfad91fb5a0c842d01fa548067e43bae446248018c48f7072c29d7d8e105cb44faa9bf79408438cf49d1fa8fe841cd2c8c'
